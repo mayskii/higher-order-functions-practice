66a9157d9012852498d040afc0ef2e1ae93da772
Maya Zakharova <mayzakharova@gmail.com>
2025-10-02 17:52:09 -0500

    add fix

--- main.py
+++ main.py
@@ -5,23 +5,32 @@ WORDS = ["jumps", "laziest", "brown", "a", "quick", "fox", "the", "dog", "over"]
 
 # find the word that is alphabetically "highest" (comes last alphabetically)
 def get_last_word_alphabetically(words):
-    pass
+    last_word = max(words)
+    return last_word
 
 # find the longest word
 def get_longest_word(words):
-    pass
+    longest = max(words, key = lambda word: len(word))
+    return longest
 
 # find the shortest word (still using max)
 # this is a little sneaky!
 def get_shortest_word(words):
-    pass
+    shortest = max(words, key = lambda word: -len(word))
+    return shortest
 
 # BONUS: now using sorted, put the jumbled sentence into this order:
 # a quick brown fox jumps over the laziest dog
 # this should still be a list of words, not a single string
 # this is a very sneaky!
 def get_ordered_words(words):
-    pass
+    WORD_ORDER = dict(
+        a=0, quick=1, brown=2, fox=3, 
+        jumps=4, over=5, the=6, laziest=7, dog=8
+    )
+
+    ordered = sorted(words, key = lambda word: WORD_ORDER[word])
+    return ordered
 
 # BONUS BONUS BONUS: Back to using max! How can we get the _first_ word
 # alphabetically in the list? This one is _very_ sneaky (and
@@ -31,7 +40,10 @@ def get_ordered_words(words):
 # curriculum! Feel no pressure to attempt this at all!
 
 def get_first_word_alphabetically(words):
-    pass
+    first = max(words, key=lambda word: tuple(-ord(c) for c in word))
+
+    return first
+
 
 def main():
     assert get_last_word_alphabetically(WORDS) == "the"
@@ -41,16 +53,15 @@ def main():
     assert get_shortest_word(WORDS) == "a"
     print("get_shortest_word PASSED!")
 
-    # BONUS
-    # assert get_ordered_words(WORDS) == [
-    #     "a", "quick", "brown", "fox", "jumps", 
-    #     "over", "the", "laziest", "dog"
-    # ]
-    # print("get_ordered_words PASSED!")
+    assert get_ordered_words(WORDS) == [
+        "a", "quick", "brown", "fox", "jumps", 
+        "over", "the", "laziest", "dog"
+    ]
+    print("get_ordered_words PASSED!")
 
-    # BONUS BONUS BONUS
-    # assert get_first_word_alphabetically(WORDS) == "a"
-    # print("get_first_word_alphabetically PASSED!")
+
+    assert get_first_word_alphabetically(WORDS) == "a"
+    print("get_first_word_alphabetically PASSED!")
 
     
     print("All tests PASSED!")
